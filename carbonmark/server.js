24539abb872e51392c9902d958ef3a5c0fba2b71
vivek1504 <jadhavvivek2743@gmail.com>
2024-09-15 16:35:50 +0530

    added-4

--- carbonmark/server.js
+++ carbonmark/server.js
@@ -1,18 +1,24 @@
 
-import { PrismaClient } from '@prisma/client';
-import express from 'express';
+const { PrismaClient } = require('@prisma/client');
+
+
+const express = require('express');
 const app = express();
 
 const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
+app.get('/',async (req, res) => {
 
     try {
-        const carbonMark = prisma.carbonMark.findMany();
+        const carbonMark =await prisma.carbonMark.findMany();
 
         return res.status(200).json(carbonMark);
     }
     catch(error){
         return res.status(500).json({error: 'Internal Server Error'});
     }
+});
+
+app.listen(3002, () => {
+    console.log('Server is running on port 3002');
 });
